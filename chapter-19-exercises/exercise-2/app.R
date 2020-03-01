f0af2834ec45555f9acc4b6a46386118f67f9ec2
LouiseTang <louisetang@LouiseTangdeMacBook-Air.local>
2020-02-29 23:21:56 -0800

    finish

--- chapter-19-exercises/exercise-2/app.R
+++ chapter-19-exercises/exercise-2/app.R
@@ -12,27 +12,25 @@ source("app_ui.R")
 # Run the app through this file.
 
 # Create a new `shinyApp()` using the loaded `ui` and `server` variables
-  textOutput(outputId = "costs")){
-server<- function(inpiut_list, output_list) {
-  
-}
-}
+shinyApp(ui = my_ui, server = my_server)
 
   
   # A `titlePanel()` layout with the text "Cost Calculator"
-my_ui <- titlePanel(
-  h2("Greetings from Shiny"),
-  textInput(inputId = "username", label = "Cost Calculato?"),
-  textOutput(outputId = "message")
-)
+my_ui <- fluidPage( 
+  titlePane("Cost Calculator"),
+  tnumericInput(inputID= "price", label="Price (in dollars)", value = 0, min=0 ),
+  tnumericInput(inputID = "quality", label="uantity", valur=1, min=1)
+  p(strong("cost")),
+  textOutput(outputId = "costs"))
+server<- function(inpiut_list, output_list) {
   
+  
+}
   # A `numericInput()` widget with the label "Price (in dollars)"
 
   # It should have a default value of 0 and a minimum value of 0
   # Hint: look up the function's arguments in the documentation!
-numericInput <- Price (input, output) {
-   min = message_str <- paste0(min)
-  }
+
   
 
   # A second `numericInput()` widget with the label "Quantity"
@@ -40,16 +38,9 @@ numericInput <- Price (input, output) {
 
   
   # The word "Cost", strongly bolded
- ui <- cost(
-  h1("My Cost")
- )
-  
+ 
   # A `textOutput()` output of a calculated value labeled `cost`
-  cost <- textOutput(
-    min = 18,                  # minimum slider value
-    max = 80,                  # maximum slider value
-    value = 42
-  )
+  
 
 
 # Define a `server` function (with appropriate arguments)
@@ -59,20 +50,8 @@ numericInput <- Price (input, output) {
   # Assign a reactive `renderText()` function to the output's `cost` value
   # The reactive expression should return the input `price` times the `quantity`
   # So it looks nice, paste a "$" in front of it!
-  my_server <- function(cost) {
   
-    output$message <- renderText({
-      
-      message_str <- paste0("Hello ", input$username, "!")
-      
-      
-      return(message_str)
-    })
-  }
 
 
 # Create a new `shinyApp()` using the above ui and server
-  ui <- fluidPage(
-    h1("My", em("Awesome"), "App"),
-    p("My app is really cool.", "It's the coolest thing ever!"),
-  )
+  
